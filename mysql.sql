fb230066eb6a724b3e8f1bbf5452575bf8d8c9b4
RahulMajukar <rahulmajukar38@gmail.com>
2025-12-01 09:51:36 +0530

    first commit with create depatment and user table in mysql

---
+++ mysql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_id INT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
+);
